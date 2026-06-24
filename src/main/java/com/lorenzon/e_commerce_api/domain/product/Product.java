04d30bd034039af28534292edadeef05bdc0e4ab
jessicalorenzon <jelorenzon@gmail.com>
2026-06-24 15:27:48 -0300

    feat: create product entity

---
+++ src/main/java/com/lorenzon/e_commerce_api/domain/product/Product.java
@@ -0,0 +1,27 @@
+package com.lorenzon.e_commerce_api.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+}
