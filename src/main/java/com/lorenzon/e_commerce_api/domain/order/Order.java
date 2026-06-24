c3d50d372352109f732998af394bfae32998e971
jessicalorenzon <jelorenzon@gmail.com>
2026-06-24 15:36:15 -0300

    feat: create order entity

---
+++ src/main/java/com/lorenzon/e_commerce_api/domain/order/Order.java
@@ -0,0 +1,27 @@
+package com.lorenzon.e_commerce_api.domain.order;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant moment;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
---
+++ src/main/java/com/lorenzon/e_commerce_api/domain/order/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.lorenzon.e_commerce_api.domain.order;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, CANCELED
+}
